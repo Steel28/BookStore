9758af980e5eeb732191a503852019213bdf2954
Steel28 <yassel.perez.19@gmail.com>
2020-06-24 10:56:21 -0500

    bookstore with cart functionalities

---
+++ src/main/java/com/example/onlinebookstore/repository/BookCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.example.onlinebookstore.repository;
+
+import com.example.onlinebookstore.entity.BookCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@RepositoryRestResource(collectionResourceRel = "bookCategory", path = "book-category")
+public interface BookCategoryRepository extends JpaRepository<BookCategory,Long> {
+}
---
+++ src/main/java/com/example/onlinebookstore/repository/BookRepository.java
@@ -0,0 +1,20 @@
+package com.example.onlinebookstore.repository;
+
+import com.example.onlinebookstore.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+public interface BookRepository extends JpaRepository <Book, Long> {
+
+    @RestResource(path = "categoryid")
+   Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    @RestResource(path = "searchbykeyword")
+    Page<Book> findByNameContaining(@Param("name") String keyword, Pageable pageable);
+
+}
